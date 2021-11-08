05239f047f236975b67666ff28a8ef104fb1ca24
frzrbox <davidefhan@hey.com>
2021-11-07 21:48:34 -1000

    Initializes project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Startline
+
+An Amagaki starter using PostCSS, Typescript and SWC
+
+## Built In-Features
+
+- [PostCSS](#postcss)
+- [Critical Styles](#critical-styles)
---
+++ amagaki.ts
@@ -0,0 +1,49 @@
+import {
+	Document,
+	NunjucksPlugin,
+	Pod,
+	StaticFile,
+	Url,
+} from "@amagaki/amagaki";
+
+module.exports = function (pod) {
+	pod.configure({
+		meta: {
+			name: "Startline",
+		},
+		localization: {
+			defaultLocale: "en",
+			locales: ["en", "fr", "it", "ja"],
+		},
+		staticRoutes: [
+			{
+				path: "/static/",
+				staticDir: "/dist/",
+			},
+			{
+				path: "/static/img/",
+				staticDir: "assets/img/",
+			},
+		],
+	});
+
+	const nunjucksPlugin = pod.plugins.get("NunjucksPlugin") as NunjucksPlugin;
+
+	type Urlable = StaticFile | Document | string;
+
+	nunjucksPlugin.addFilter("url", function (object: Urlable) {
+		if (object instanceof StaticFile) {
+			return `${Url.relative(object.url.path, this.ctx.doc)}?fingerprint=${
+				object.fingerprint
+			}`;
+		} else if (object instanceof Document) {
+			return `${Url.relative(object.url.path, this.ctx.doc)}`;
+		}
+		return `${Url.relative(object, this.ctx.doc)}`;
+	});
+
+	nunucksPlugin.addFilter("log", function (value: any) {
+		console.log("log", value);
+	});
+};
+j;
